11ac9e3465b453e05eed0eed77402cb3efeee1d2
huy8895 <huytv.ous@mcredit.com.vn>
2022-09-14 10:49:20 +0700

    mang hai chieu

--- php-training/index.php
+++ php-training/index.php
@@ -117,5 +117,24 @@ foreach ($data as $key => $val) {
 
 ?>
 
+<?php
+$cars = array (
+    array("Volvo",22,18),
+    array("BMW",15,13),
+    array("Saab",5,2),
+    array("Land Rover",17,15)
+);
+
+echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
+
+echo '<br>';
+foreach ($cars as $key => $value) {
+    echo 'key: '.$key .' - value: ' . "<br/>";
+
+}
+?>
 </body>
 </html>
